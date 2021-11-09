aed7ae8428e5a4133bde843a10f0b2d7d2838937
cturn5830 <91220538+cturn5830@users.noreply.github.com>
2021-11-09 13:11:47 -0500

    Update Forms.js

--- components/Forms.js
+++ components/Forms.js
@@ -1,13 +1,5 @@
 import router, { useRouter } from "next/router";
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwLpHPedIqGTXRlj-oL8XCIz_iPZICsbyhYGrOT6WeyNvQ4QZG1cTioaJBcoy5iggQ8/exec'
-const form = document.forms['submit-to-google-sheet']
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => console.log('Success!', response))
-    .catch(error => console.error('Error!', error.message))
-})
 export default function Form(){
 
     return(

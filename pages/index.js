5e0ee31c8240f0509a290cd28243db3d015998bf
cturn5830 <91220538+cturn5830@users.noreply.github.com>
2021-11-09 12:34:23 -0500

    Update index.js
    '   ' to "    "

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import styles from '../styles/Home.module.css'
 import Titleimg from "../components/Titleimg.js"
 import Content from "../components/Content"
 import Forms from "../components/Forms"
-import Bikeb from '../components/Bike'
+import Bikeb from "../components/Bike"
 
 export default function Home() {
   return (

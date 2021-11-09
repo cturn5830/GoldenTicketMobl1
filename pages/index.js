aa7cbb9c4b64433eaecafdbba6d03b9a5619b36a
cturn5830 <91220538+cturn5830@users.noreply.github.com>
2021-11-09 12:28:13 -0500

    test no bike

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,13 @@ import styles from '../styles/Home.module.css'
 import Titleimg from "../components/Titleimg.js"
 import Content from "../components/Content"
 import Forms from "../components/Forms"
-import Biketext from '../components/Bike'
+
 
 export default function Home() {
   return (
     <div className={styles.container}>
         <Titleimg />
-        <Biketext />
+
         <Content/>
         <Forms />
     </div>

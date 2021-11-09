d84ea82a97bebd5ad46c396025fdee96c6e58035
cturn5830 <91220538+cturn5830@users.noreply.github.com>
2021-11-09 11:26:17 -0500

    Changed css
    Changed scss request to css request

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-import "../styles/globals.scss";
+import "../styles/globals.css";
 import * as ga from "../lib/ga";
 
 function MyApp({ Component, pageProps }) {

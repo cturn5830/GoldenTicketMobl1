99af9ce9d80d8be536f92aee4dbd6bd1c9d3ec2d
cturn5830 <91220538+cturn5830@users.noreply.github.com>
2021-11-09 11:44:51 -0500

    Removed submit mail for test
    Removed submit mail for test

--- components/Forms.js
+++ components/Forms.js
@@ -7,7 +7,7 @@ export default function Form(){
                 <input type="text" id="fname" name="fname" placeholder = "First Name"/> <br/>
                 <input type="text" id="lname" name="lname" placeholder = "Last Name"/>  <br/>
                 <input type="text" id="email" name="email" placeholder = "E-Mail"/>  <br/>
-               <span className="submits"> <input type="submit" id ="submit"value="Submit" onClick="location.href='mailto:${document.getElementById(`email`)}';" onClick={()=>router.push("/thank")}></input></span>
+               <span className="submits"> <input type="submit" id ="submit"value="Submit" onClick={()=>router.push("/thank")}></input></span>
             </form>
             <p>
             Winners will be notified Nov. 3 via text and email, with only one

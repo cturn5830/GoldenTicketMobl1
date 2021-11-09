c5b3866ac18fd46b7c525f54df7833be3217f93f
cturn5830 <91220538+cturn5830@users.noreply.github.com>
2021-11-09 13:22:24 -0500

    Update Forms.js

--- components/Forms.js
+++ components/Forms.js
@@ -1,24 +1,23 @@
 import router, { useRouter } from "next/router";
 
 export default function Form(){
-function postform(){
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbwLpHPedIqGTXRlj-oL8XCIz_iPZICsbyhYGrOT6WeyNvQ4QZG1cTioaJBcoy5iggQ8/exec'
-    const form = document.forms['submit-to-google-sheet']
-  
-    form.addEventListener('submit', e => {
-      e.preventDefault()
-      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-        .then(response => console.log('Success!', response))
-        .catch(error => console.error('Error!', error.message))
-    })
-}
+function postform()
     return(
         <div className="forms">
-            <form >
+            <form name="submit-to-google-sheet">
                 <input type="text" id="fname" name="fname" placeholder = "First Name"/> <br/>
                 <input type="text" id="lname" name="lname" placeholder = "Last Name"/>  <br/>
                 <input type="text" id="email" name="email" placeholder = "E-Mail"/>  <br/>
-                <input type="submit" id ="submit"value="Submit" onClick={postform}></input>
+                <input type="submit" id ="submit"value="Submit" onClick={()=>{
+                            const scriptURL = 'https://script.google.com/macros/s/AKfycbwLpHPedIqGTXRlj-oL8XCIz_iPZICsbyhYGrOT6WeyNvQ4QZG1cTioaJBcoy5iggQ8/exec'
+                           const form = document.forms['submit-to-google-sheet']
+
+                            
+                              fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+                                .then(response => console.log('Success!', response))
+                                .catch(error => console.error('Error!', error.message))
+                            
+}}></input>
             </form>
             <p>
             Winners will be notified Nov. 3 via text and email, with only one

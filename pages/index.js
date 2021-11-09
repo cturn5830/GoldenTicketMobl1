8bf8e575510beb4c572a8be633ad07f57cbd9852
cturn5830 <91220538+cturn5830@users.noreply.github.com>
2021-11-09 12:26:05 -0500

    import image in index
    import image in index

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Image from 'next/dist/client/image'
 import styles from '../styles/Home.module.css'
 import Titleimg from "../components/Titleimg.js"
 import Content from "../components/Content"

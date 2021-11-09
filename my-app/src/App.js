49f5593d7a0aa96facad9d9004aaaf5cc84606bf
cturn5830 <91220538+cturn5830@users.noreply.github.com>
2021-11-09 11:07:25 -0500

    First push
    First push

---
+++ my-app/src/App.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import "../styles/globals.scss";
+import * as ga from "../lib/ga";
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  });
+
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;

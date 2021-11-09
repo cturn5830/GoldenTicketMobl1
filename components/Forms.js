fd6d7e811bb35e2d3d18680c6291e89aacdb8d85
cturn5830 <91220538+cturn5830@users.noreply.github.com>
2021-11-09 13:55:50 -0500

    Update Forms.js

--- components/Forms.js
+++ components/Forms.js
@@ -7,7 +7,7 @@ export default function Form(){
                 <input type="text" id="fname" name="fname" placeholder = "First Name"/> <br/>
                 <input type="text" id="lname" name="lname" placeholder = "Last Name"/>  <br/>
                 <input type="text" id="email" name="email" placeholder = "E-Mail"/>  <br/>
-                <input type="submit" id ="submitbtn"value="Submit" onClick={()=>{
+                <input type="submit" id ="submit"value="Submit" onClick={()=>{
                             const scriptURL = 'https://script.google.com/macros/s/AKfycbwLpHPedIqGTXRlj-oL8XCIz_iPZICsbyhYGrOT6WeyNvQ4QZG1cTioaJBcoy5iggQ8/exec'
                            const form = document.forms['submit-to-google-sheet']
 
@@ -15,7 +15,8 @@ export default function Form(){
                               fetch(scriptURL, { method: 'POST', body: new FormData(form)})
                                 .then(response => console.log('Success!', response))
                                 .catch(error => console.error('Error!', error.message))
-}}></input>
+},()=>router.push("/thank")}
+></input>
             </form>
             <p>
             Winners will be notified Nov. 3 via text and email, with only one

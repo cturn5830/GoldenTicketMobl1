a46a2b8492baa57d25a2f7fe8189ba1fedec3354
cturn5830 <91220538+cturn5830@users.noreply.github.com>
2021-11-09 14:03:53 -0500

    Update Forms.js

--- components/Forms.js
+++ components/Forms.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import Router from 'next/router'
-import Link from `next/link`
+import Link from "next/link"
 
 export default function Form(){
     return(

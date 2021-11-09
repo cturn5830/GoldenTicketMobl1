6ad49443cf5eb6b9f4aff77e43a86b12e2ea7531
cturn5830 <91220538+cturn5830@users.noreply.github.com>
2021-11-09 12:33:01 -0500

    Bike fix
    Bike fix

--- components/Bike.js
+++ components/Bike.js
@@ -1,16 +1,11 @@
 import Image from "next/image";
-import Bike from "../public/motorcycle.jpg"
+import Motor from "../public/motorcycle.jpg"
 
-export default function Biketext(){
+export default function Bikeb(){
     return(
-        <div className="Heroimg"style={{  
-            
-            backgroundPosition: 'center',
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat'
-          }}>
+        <div>
               
-            <Image src={Bike} alt="Counts Kustoms Motorcycle"/>
+            <Image src={Motor} alt="Counts Kustoms Motorcycle"/>
             
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,14 @@
-import Image from 'next/dist/client/image'
 import styles from '../styles/Home.module.css'
 import Titleimg from "../components/Titleimg.js"
 import Content from "../components/Content"
 import Forms from "../components/Forms"
-
+import Bikeb from '../components/Bike'
 
 export default function Home() {
   return (
     <div className={styles.container}>
         <Titleimg />
-
+        <Bikeb/>
         <Content/>
         <Forms />
     </div>

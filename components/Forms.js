70b552d9c4b9d174b207db5e31742106dc3d59b2
cturn5830 <91220538+cturn5830@users.noreply.github.com>
2021-11-10 14:00:49 -0500

    Form changes

--- components/Forms.js
+++ components/Forms.js
@@ -10,7 +10,7 @@ export default function Form(){
                 <input type="text" id="Phone#" name="Phone#" placeholder = "Phone Number"/> <p></p>
                 <input type="text" id="email" name="email" placeholder = "E-Mail"/>  <p></p>
                 <input type="text" id="ZipCode" name="ZipCode" placeholder = "Zip Code"/><p> </p>
-                <input type="checkbox" id="OPTin" name="OPTin"/><p>
+                <input type="checkbox" id="OPTin" name="OPTin"/>
                 I want to subscribe to get future offers and news from ExxonMobil,
               and I agree for my submitted information to be used in accordance
               with the{" "}
@@ -22,7 +22,7 @@ export default function Form(){
               >
                 ExxonMobil privacy policy.
               </a>
-                </p>
+                
             <Link href="/thank"><input type="submit" id ="submit"value="Submit" onClick={()=>{
                             const scriptURL = 'https://script.google.com/macros/s/AKfycbwLpHPedIqGTXRlj-oL8XCIz_iPZICsbyhYGrOT6WeyNvQ4QZG1cTioaJBcoy5iggQ8/exec'
                            const form = document.forms['submit-to-google-sheet']

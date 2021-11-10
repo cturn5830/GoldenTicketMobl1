17903e391198f9e9de9b7c9a6801384724417562
cturn5830 <91220538+cturn5830@users.noreply.github.com>
2021-11-10 14:23:41 -0500

    Update Forms.js

--- components/Forms.js
+++ components/Forms.js
@@ -12,7 +12,7 @@ export default function Form(){
                 <input type="text" id="Phone#" name="Phone#" placeholder = "Phone Number"/> <p></p>
                 <input type="text" id="email" name="email" placeholder = "E-Mail"/>  <p></p>
                 <input type="text" id="ZipCode" name="ZipCode" placeholder = "Zip Code"/><p> </p>
-                <input type="checkbox" id="OPTin" name="OPTin"/> {disclaimer}
+                <input type="checkbox" id="OPTin" name="OPTin"/> {{disclaimer}}
               <br/>
             <Link href="/thank"><input type="submit" id ="submit"value="Submit" onClick={()=>{
                             const scriptURL = 'https://script.google.com/macros/s/AKfycbwLpHPedIqGTXRlj-oL8XCIz_iPZICsbyhYGrOT6WeyNvQ4QZG1cTioaJBcoy5iggQ8/exec'

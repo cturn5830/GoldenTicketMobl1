f1c51bf975e1bb26805d458578f4c37356090d7f
cturn5830 <91220538+cturn5830@users.noreply.github.com>
2021-11-10 14:20:41 -0500

    Update Forms.js

--- components/Forms.js
+++ components/Forms.js
@@ -2,7 +2,8 @@ import React, { useEffect } from "react";
 import Link from "next/link"
 
 export default function Form(){
-    const disclaimer = <a href="https://corporate.exxonmobil.com/Global-legal-pages/privacy-policy" rel="noreferrer">ExxonMobil privacy policy. </a>
+    const words ="I want to subscribe to get future offers and news from ExxonMobil, and I agree for my submitted information to be used in accordance with the"
+    const disclaimer = words + <a href="https://corporate.exxonmobil.com/Global-legal-pages/privacy-policy" rel="noreferrer">ExxonMobil privacy policy. </a>
     return(
         <div>
             <form name="submit-to-google-sheet">
@@ -11,13 +12,8 @@ export default function Form(){
                 <input type="text" id="Phone#" name="Phone#" placeholder = "Phone Number"/> <p></p>
                 <input type="text" id="email" name="email" placeholder = "E-Mail"/>  <p></p>
                 <input type="text" id="ZipCode" name="ZipCode" placeholder = "Zip Code"/><p> </p>
-                <input type="checkbox" id="OPTin" name="OPTin"/><div>
-                I want to subscribe to get future offers and news from ExxonMobil,
-              and I agree for my submitted information to be used in accordance
-              with the {disclaimer}
-              
-              </div>
-                <p></p>
+                <input type="checkbox" id="OPTin" name="OPTin"/> {disclaimer}
+              <br/>
             <Link href="/thank"><input type="submit" id ="submit"value="Submit" onClick={()=>{
                             const scriptURL = 'https://script.google.com/macros/s/AKfycbwLpHPedIqGTXRlj-oL8XCIz_iPZICsbyhYGrOT6WeyNvQ4QZG1cTioaJBcoy5iggQ8/exec'
                            const form = document.forms['submit-to-google-sheet']

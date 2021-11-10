b1f2dbd82df9f3faa8e3339103ec7a966f58ffea
cturn5830 <91220538+cturn5830@users.noreply.github.com>
2021-11-10 09:11:47 -0500

    Update Content.js

--- components/Content.js
+++ components/Content.js
@@ -3,8 +3,8 @@ export default function Content(){
         <div className="ContentDiv">
         <h2>Thank you for your purchase!</h2>
         <p>
-          Enter for your chance to win a Golden Ticket for two to the Mobil 1™ x
-          Count's Kustoms VIP event.*
+          {`Enter for your chance to win a Golden Ticket for two to the Mobil 1™ x
+          Count's Kustoms VIP event.*`}
         </p>
       </div>
     )

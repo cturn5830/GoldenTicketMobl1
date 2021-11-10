a21ddae76f36b5e90b2323be696f1628387001e8
cturn5830 <91220538+cturn5830@users.noreply.github.com>
2021-11-10 13:45:39 -0500

    Revised form
    Revised form

--- components/Forms.js
+++ components/Forms.js
@@ -1,14 +1,28 @@
 import React, { useEffect } from "react";
-import Router from 'next/router'
 import Link from "next/link"
 
 export default function Form(){
     return(
         <div>
             <form name="submit-to-google-sheet">
-                <input type="text" id="fname" name="fname" placeholder = "First Name"/> <br/>
-                <input type="text" id="lname" name="lname" placeholder = "Last Name"/>  <br/>
-                <input type="text" id="email" name="email" placeholder = "E-Mail"/>  <br/>
+                <input type="text" id="fname" name="fname" placeholder = "First Name"/> <p></p>
+                <input type="text" id="lname" name="lname" placeholder = "Last Name"/>  <p></p>
+                <input type="text" id="Phone#" name="Phone#" placeholder = "Phone Number"/> <p></p>
+                <input type="text" id="email" name="email" placeholder = "E-Mail"/>  <p></p>
+                <input type="text" id="ZipCode" name="ZipCode" placeholder = "Zip Code"/><p> </p>
+                <input type="checkbox" id="OPTin" name="OPTin"/><p>
+                I want to subscribe to get future offers and news from ExxonMobil,
+              and I agree for my submitted information to be used in accordance
+              with the{" "}
+              <a
+                className="underline text-reflex-600"
+                href="https://corporate.exxonmobil.com/Global-legal-pages/privacy-policy"
+                target="_blank"
+                rel="noreferrer"
+              >
+                ExxonMobil privacy policy.
+              </a>
+                </p>
             <Link href="/thank"><input type="submit" id ="submit"value="Submit" onClick={()=>{
                             const scriptURL = 'https://script.google.com/macros/s/AKfycbwLpHPedIqGTXRlj-oL8XCIz_iPZICsbyhYGrOT6WeyNvQ4QZG1cTioaJBcoy5iggQ8/exec'
                            const form = document.forms['submit-to-google-sheet']
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import React, { useEffect } from "react";
-import Router from 'next/router'
-
 
 import styles from '../styles/Home.module.css'
 import Titleimg from "../components/Titleimg.js"

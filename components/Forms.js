0d663688892e08f7e330f440117eedeb3ed0126a
cturn5830 <91220538+cturn5830@users.noreply.github.com>
2021-11-10 14:27:37 -0500

    Update Forms.js

--- components/Forms.js
+++ components/Forms.js
@@ -3,7 +3,8 @@ import Link from "next/link"
 
 export default function Form(){
     const words ="I want to subscribe to get future offers and news from ExxonMobil, and I agree for my submitted information to be used in accordance with the"
-    const disclaimer = words + '<a href="https://corporate.exxonmobil.com/Global-legal-pages/privacy-policy" rel="noreferrer">ExxonMobil privacy policy. </a>'
+    const disclaimlink = <a href="https://corporate.exxonmobil.com/Global-legal-pages/privacy-policy" rel="noreferrer">ExxonMobil privacy policy.</a>
+    const disclaimerRender = words + disclaimlink
     return(
         <div>
             <form name="submit-to-google-sheet">
@@ -12,7 +13,7 @@ export default function Form(){
                 <input type="text" id="Phone#" name="Phone#" placeholder = "Phone Number"/> <p></p>
                 <input type="text" id="email" name="email" placeholder = "E-Mail"/>  <p></p>
                 <input type="text" id="ZipCode" name="ZipCode" placeholder = "Zip Code"/><p> </p>
-                <input type="checkbox" id="OPTin" name="OPTin"/> {disclaimer}
+                <input type="checkbox" id="OPTin" name="OPTin"/> {disclaimerRender}
               <br/>
             <Link href="/thank"><input type="submit" id ="submit"value="Submit" onClick={()=>{
                             const scriptURL = 'https://script.google.com/macros/s/AKfycbwLpHPedIqGTXRlj-oL8XCIz_iPZICsbyhYGrOT6WeyNvQ4QZG1cTioaJBcoy5iggQ8/exec'

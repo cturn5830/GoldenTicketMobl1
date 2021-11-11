1d80520d938ca5c36e40a491d3e16ebc273d1d76
cturn5830 <91220538+cturn5830@users.noreply.github.com>
2021-11-11 11:06:26 -0500

    Update Thankscontent.js
    target fix

--- components/Thankscontent.js
+++ components/Thankscontent.js
@@ -9,7 +9,7 @@ export default function Thankscontent() {
           notified by text and email by November 3.`}
         </p>
         <h3>Ride on. And on.</h3>
-        <a target="_blank" href="https://www.mobil.com/en/lubricants/for-personal-vehicles/motorcycles">
+        <a href="https://www.mobil.com/en/lubricants/for-personal-vehicles/motorcycles" target="_blank">
         <button className="thanksbtn">
           Experience the difference
         </button>
